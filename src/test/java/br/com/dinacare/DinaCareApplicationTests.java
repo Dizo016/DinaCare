c55e452d14cb4bb0ff581902d3a9c08e116f68e7
Diego Silva <diegossilva2008@gmail.com>
2026-02-02 16:08:33 -0300

    Initial commit - DinaCare backend MVP

---
+++ src/test/java/br/com/dinacare/DinaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dinacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinaCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
